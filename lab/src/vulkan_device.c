902e6a6bb8c74f1aa3357a831799ca98c7b1e66c
Nick DuPlessis <nduplessis15@gmail.com>
2024-10-09 11:08:06 -0400

    Create vk device

--- lab/src/vulkan_device.c
+++ lab/src/vulkan_device.c
@@ -12,6 +12,7 @@ result_t get_vk_physical_device(vulkan_context_t *context) {
   VkPhysicalDeviceProperties physical_device_props = {0};
   VkQueueFamilyProperties queue_family_props[32];
   u32 queue_family_prop_count = 0;
+  VkDevice device = {0};
 
   vk_result = vkEnumeratePhysicalDevices(context->instance,
                                          &physical_device_count, NULL);
@@ -23,7 +24,7 @@ result_t get_vk_physical_device(vulkan_context_t *context) {
       context->instance, &physical_device_count, &physical_device);
   result = check_vk_result(vk_result);
 
-  fprintf(stdout, "Physical Device Created\n");
+  fprintf(stdout, "Physical Device Found\n");
 
   vkGetPhysicalDeviceProperties(physical_device, &physical_device_props);
   fprintf(stdout,
@@ -41,25 +42,35 @@ result_t get_vk_physical_device(vulkan_context_t *context) {
   vkGetPhysicalDeviceQueueFamilyProperties(
       physical_device, &queue_family_prop_count, queue_family_props);
 
-  // `i` is `queueFamilyIndex` for `VkDeviceQueueCreateInfo`
+  u8 queue_family_index;
   for (int i = 0; i < queue_family_prop_count; i++) {
-    fprintf(stdout, "Queue Family Index %u Properties: %u\n", i,
-            queue_family_props[i].queueCount);
+    if (queue_family_props[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+      fprintf(stdout, "Found graphics queue family at index: %u\n", i);
+      queue_family_index = i;
 
-    float priorities[32];
-    for (int i = 0; i < 32; ++i) {
-      priorities[i] = 1.0f;
+      fprintf(stdout, "Queue Family Index %u Properties: %u\n", i,
+              queue_family_props[i].queueCount);
+      break;
     }
-
-    VkDeviceQueueCreateInfo queue_create_info = {};
-    queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-    queue_create_info.queueFamilyIndex = i;
-    queue_create_info.queueCount = queue_family_props[i].queueCount;
-    queue_create_info.pQueuePriorities = priorities;
   }
+
+  float queue_priority = 1.0f;
+  VkDeviceQueueCreateInfo queue_create_info = {};
+  queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+  queue_create_info.queueFamilyIndex = queue_family_index;
+  queue_create_info.queueCount = 1;
+  queue_create_info.pQueuePriorities = &queue_priority;
 
   VkDeviceCreateInfo device_create_info = {};
   device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+  device_create_info.queueCreateInfoCount = 1;
+  device_create_info.pQueueCreateInfos = &queue_create_info;
+
+  vk_result =
+      vkCreateDevice(physical_device, &device_create_info, NULL, &device);
+  result = check_vk_result(vk_result);
+
+  fprintf(stdout, "Device Created\n");
 
   return result;
 }

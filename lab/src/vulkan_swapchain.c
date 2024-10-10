443d8a695aab01a4dc48a1174f5458356e4ac6f2
Nick DuPlessis <nduplessis15@gmail.com>
2024-10-10 13:03:00 -0400

    Start swapchain create info struct

--- lab/src/vulkan_swapchain.c
+++ lab/src/vulkan_swapchain.c
@@ -1,9 +1,16 @@
 #include "vulkan_swapchain.h"
 #include <stdio.h>
+#include <vulkan/vulkan_core.h>
 
 result_t create_swapchain(vulkan_context_t *context) {
   result_t result = {.success = TRUE, .message = NULL};
   fprintf(stdout, "Creating swapchain...\n");
+
+  VkSwapchainKHR swapchain = {0};
+
+  VkSwapchainCreateInfoKHR create_info = {0};
+  create_info.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+  create_info.surface = context->surface;
 
   return result;
 }

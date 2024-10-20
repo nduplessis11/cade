15a1b8f18337a6bdf52b51aaa9381c05e8551337
Nick DuPlessis <nduplessis15@gmail.com>
2024-10-19 20:42:29 -0400

    Define rough platform interface

--- lab/src/platform.h
+++ lab/src/platform.h
@@ -5,3 +5,12 @@
 #if defined(_WIN32)
 #define CADE_PLATFORM_WIN32 1
 #endif
+
+// https://github.com/KhronosGroup/Vulkan-Loader/blob/main/docs/LoaderDriverInterface.md
+
+// https://github.com/libsdl-org/SDL/blob/main/src/video/x11/SDL_x11vulkan.c#L172
+// Create surface using device and window
+void platform_create_surface();
+
+
+void platform_pump_events();
